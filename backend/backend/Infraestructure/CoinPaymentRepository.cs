ecf004b92bf83ee6f7942d0c41fe2c6d0e6eb65d
24danielcr <24danielcr@gmail.com>
2025-07-12 10:38:35 -0600

    Coin payment command and repository implementation.

---
+++ backend/backend/Infraestructure/CoinPaymentRepository.cs
@@ -0,0 +1,32 @@
+﻿using backend.Database;
+using backend.Domain;
+
+namespace backend.Infraestructure
+{
+  public class CoinPaymentRepository : ICoinPaymentRepository
+  {
+    private readonly VendingMachineDB vendingMachineDB;
+
+    public CoinPaymentRepository(VendingMachineDB vendingMachineDB)
+    {
+      this.vendingMachineDB = vendingMachineDB;
+    }
+
+    public void ProcessCoins(List<CoinPaymentModel> coins)
+    {
+      var vendingMachineMoney = vendingMachineDB.vendingMachineMoney;
+
+      foreach (var coinPayment in coins)
+      {
+        CoinDenomination denomination = (CoinDenomination) coinPayment.Value;
+        vendingMachineMoney[denomination].Quantity -= coinPayment.Quantity;
+      }
+    }
+
+    public Dictionary<CoinDenomination, VendingMachineMoneyModel>
+      GetAvailableVendingMachineCoins()
+    {
+      return vendingMachineDB.vendingMachineMoney;
+    }
+  }
+}
