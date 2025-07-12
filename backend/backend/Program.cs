6966af44f37261cf1fd11ad3399673b823f064c0
24danielcr <24danielcr@gmail.com>
2025-07-12 10:40:22 -0600

    Scoped values to Program

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,4 +1,6 @@
-﻿using backend.Database;
+﻿using backend.Application;
+using backend.Database;
+using backend.Infraestructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPaymentOrchestrator, PaymentOrchestrator>();
+
+builder.Services.AddScoped<IProductPaymentCommand, ProductPaymentCommand>();
+
+builder.Services.AddScoped<ICoinPaymentCommand, CoinPaymentCommand>();
+
+builder.Services.AddScoped<ICoinPaymentRepository, CoinPaymentRepository>();
+
+builder.Services.AddScoped<IPaymentOrchestratorRepository
+  , PaymentOrchestratorRepository>();
+
+builder.Services.AddScoped<IProductPaymentRepository
+  , ProductPaymentRepository>();
+
+builder.Services.AddScoped<IProductPaymentRepository
+  , ProductPaymentRepository>();
 
 builder.Services.AddSingleton<VendingMachineDB>();
 
