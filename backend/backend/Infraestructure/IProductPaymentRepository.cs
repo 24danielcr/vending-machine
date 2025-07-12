27d7c68ae92c539f52947db303359598f957f287
24danielcr <24danielcr@gmail.com>
2025-07-12 10:39:18 -0600

    Product payment command and repository implementation.

---
+++ backend/backend/Infraestructure/IProductPaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using backend.Domain;
+
+namespace backend.Infraestructure
+{
+  public interface IProductPaymentRepository
+  {
+    public void BuyProducts(List<ProductPaymentModel> products);
+
+    public int GetProductQuantity(string productName);
+  }
+}
